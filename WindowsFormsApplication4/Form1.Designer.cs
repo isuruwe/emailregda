b5e512e18575b8877e607440258038fc67a72c81
SLAF <SLAF@AFHQ-ITU-SOFT1>
2023-04-25 14:23:44 +0530

    dareg

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ERMSDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ERMSDataSet = new WindowsFormsApplication4.ERMSDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button2 = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ERMSDataTableAdapter = new WindowsFormsApplication4.ERMSDataSetTableAdapters.ERMSDataTableAdapter();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ERMSDataBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ERMSDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ERMSDataBindingSource
+            // 
+            this.ERMSDataBindingSource.DataMember = "ERMSData";
+            this.ERMSDataBindingSource.DataSource = this.ERMSDataSet;
+            // 
+            // ERMSDataSet
+            // 
+            this.ERMSDataSet.DataSetName = "ERMSDataSet";
+            this.ERMSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(127, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 7, 24, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(807, 65);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.Value = new System.DateTime(2018, 7, 24, 0, 0, 0, 0);
+            this.dateTimePicker2.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Print";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // reportViewer1
+            // 
+            reportDataSource4.Name = "DataSet1";
+            reportDataSource4.Value = this.ERMSDataBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApplication4.rptereg.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(42, 85);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(1183, 542);
+            this.reportViewer1.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(882, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // ERMSDataTableAdapter
+            // 
+            this.ERMSDataTableAdapter.ClearBeforeFill = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DELIVER",
+            "SEND"});
+            this.comboBox1.Location = new System.Drawing.Point(357, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "DELIVER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 639);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Register";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ERMSDataBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ERMSDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button2;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ERMSDataBindingSource;
+        private ERMSDataSet ERMSDataSet;
+        private ERMSDataSetTableAdapters.ERMSDataTableAdapter ERMSDataTableAdapter;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Drawing.Printing;
+using Microsoft.Reporting.WinForms;
+using System.Configuration;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int printRow = 0;
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'ERMSDataSet.ERMSData' table. You can move, or remove it, as needed.
+            //this.ERMSDataTableAdapter.Fill(this.ERMSDataSet.ERMSData);
+            this.dateTimePicker1.Value = DateTime.Now.Date;
+            this.dateTimePicker2.Value = DateTime.Now.Date;
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Sender");
+            dt.Columns.Add("MessageSubject");
+            dt.Columns.Add("Timestamp");
+           
+            //this.reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String fdate = dateTimePicker1.Value.Date.Date.ToShortDateString().ToString();
+            String sdate = dateTimePicker1.Value.ToString();
+            String adate = fdate+ " 23:59:59 PM";
+            String emnai = ConfigurationManager.AppSettings["setting1"];
+            int cnt=1;
+            string un = @"slaf\mailadmin";
+            System.Security.SecureString pw = new System.Security.SecureString();
+            string password = "mail@123";
+            string databaseName = "dit";
+            string exchangeServerName = "http://EXCHANGE-HA-02.slaf.int/powershell";
+            //win-6k7ld7joseu
+            string microsoftSchemaName = "http://schemas.microsoft.com/powershell/Microsoft.Exchange";
+            foreach (char ch in password)
+            {
+                pw.AppendChar(ch);
+            }
+            PSCredential cred = new PSCredential(un, pw);
+            DataTable dstIntData = new DataTable();
+            if (dstIntData!=null)
+            {
+                dstIntData.Clear();
+            }
+            if (ds != null)
+            {
+                ds.Tables.Clear();
+            }
+            if (dt != null)
+            {
+                dt.Rows.Clear();
+            }
+            progressBar1.Visible = true;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            WSManConnectionInfo connectionInfo = new WSManConnectionInfo(new Uri(exchangeServerName), microsoftSchemaName, cred);
+            connectionInfo.AuthenticationMechanism = AuthenticationMechanism.Kerberos;
+            // dataGridView1.Rows.Clear();
+            String cmbtest = comboBox1.SelectedItem.ToString();
+
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+                using (PowerShell powershell = PowerShell.Create())
+                {
+                    powershell.AddCommand("Get-MessageTrackingLog");
+                    powershell.AddParameter("eventid", cmbtest);
+                    powershell.AddParameter("Start", sdate);
+                    powershell.AddParameter("End", adate);
+                    powershell.AddParameter("Recipient", emnai);
+                    powershell.AddCommand("select-object");
+                    var props = new string[] { "Sender", "MessageSubject", "TimeStamp" };
+                    powershell.AddParameter("property", props);
+                    runspace.Open();
+                    powershell.Runspace = runspace;
+                   
+                    Collection<PSObject> results = powershell.Invoke();
+                   
+                    
+                    foreach (PSObject result in results)
+                    {
+
+                      
+
+                        DataRow dr = dt.NewRow();
+                        dr["ID"] = cnt.ToString();
+                        foreach (PSPropertyInfo prop in result.Properties)
+                            {
+                                var count = prop.Name;
+                                var name = prop.Value;
+                           
+                            //MessageBox.Show(count.ToString());
+                            //In other words generate output as you desire.
+                            // dt.Rows.Add(count, name);
+                            if (count == "Sender")
+                            {
+                                 //Create New Row
+                                dr["Sender"] = name;
+                                
+                            }
+                            if (count == "MessageSubject")
+                            {
+                                //Create New Row
+                                dr["MessageSubject"] = name;
+                                
+                            }
+                            
+                            if (count == "Timestamp")
+                            {
+                                //Create New Row
+                                dr["Timestamp"] = name;
+                               
+                              
+                            }
+                           
+                            // Set Column Value
+                           
+                        }
+                        dt.Rows.Add(dr);
+                        cnt++;
+
+
+
+
+
+                    }
+
+                   
+                    ds.Tables.Add(dt);
+
+                    dstIntData = ds.Tables[0];
+                    //dstIntData = oBOCBF_TXN_DETAIL.SelectForSavingsReport(null, this.dateTimePicker1.Text);
+                    reportViewer1.LocalReport.DataSources.Clear();
+                    ReportDataSource IntData_rsource = new ReportDataSource("DataSet1", dstIntData);
+                    reportViewer1.LocalReport.DataSources.Add(IntData_rsource);
+
+                    this.reportViewer1.RefreshReport();
+                    progressBar1.Visible = false;
+
+
+                    //for (int count = 0; count < ds.Tables[0].Rows.Count; count++)
+                    //{
+                    //    dataGridView1.Rows.Add();
+                    //    dataGridView1.Rows[count].Cells["Sender"].Value = ds.Tables[0].Rows[count]["Sender"].ToString();
+                    //    dataGridView1.Rows[count].Cells["MessageSubject"].Value = ds.Tables[0].Rows[count]["MessageSubject"].ToString();
+                    //    dataGridView1.Rows[count].Cells["Timestamp"].Value = ds.Tables[0].Rows[count]["Timestamp"].ToString();
+                       
+
+                    //}
+                }
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // printDocument1.Print();
+            PrintDocument document = new PrintDocument();
+            document.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+
+            PrintPreviewDialog ppDialog = new PrintPreviewDialog();
+            ppDialog.Document = document;
+            ppDialog.Show();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            // Bitmap bm =new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            //dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+            //e.Graphics.DrawImage(bm, 0, 0);
+
+            PrintDocument document = (PrintDocument)sender;
+            Graphics g = e.Graphics;
+
+            Brush brush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(brush);
+            Font font = new Font("Arial", 10, FontStyle.Bold);
+            Font fonte = new Font("Arial", 15, FontStyle.Bold);
+            int x = 0, y = 0, width = 300, height = 30;
+
+            SizeF sizeeee = g.MeasureString("TIME :: ", fonte);
+            float xPaddingeee = (width - sizeeee.Width) / 2;
+            g.DrawString("TIME :: ", fonte, brush, x + xPaddingeee, y + 5);
+            x += width;
+
+
+            SizeF sizee = g.MeasureString(DateTime.Now.ToString(), fonte);
+            float xPaddinge = (width - sizee.Width) / 2;
+
+            g.DrawString(DateTime.Now.ToString(), fonte, brush, x + xPaddinge, y + 5);
+            x += width;
+
+            for (int kk = 0; kk < 2; kk++)
+            {
+                SizeF sizeee = g.MeasureString("", font);
+                float xPaddingee = (width - sizee.Width) / 2;
+
+                g.DrawString("", font, brush, x + xPaddingee, y + 5);
+                x += width;
+            }
+            x = 0;
+            y += 60;
+
+
+            foreach (DataColumn column in dt.Columns)
+            {
+                g.DrawRectangle(pen, x, y, width, height);
+                SizeF size = g.MeasureString(column.ColumnName, fonte);
+                float xPadding = (width - size.Width) / 2;
+
+                g.DrawString(column.ColumnName, fonte, brush, x + xPadding, y + 5);
+                x += width;
+            }
+
+
+            x = 0;
+            y += 30;
+            int columnCount = dt.Columns.Count;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    g.DrawRectangle(pen, x, y, width, height);
+                    SizeF size = g.MeasureString(row[i].ToString(), font);
+                    float xPadding = (width - size.Width) / 2;
+
+                    g.DrawString(row[i].ToString(), font, brush, x + xPadding, y + 5);
+                    x += width;
+                }
+                x = 0;
+                y += 30;
+            }
+
+
+        }
+    }
+}
